965531c7d16110dadf678a71c26c708138450992
Elena Calcada Evangelista <calcada.elena@gmail.com>
2023-01-12 08:52:03 -0300

    validate schema create-doctor-controller with zod

--- src/modules/doctor/useCases/create-doctor/create-doctor.controller.ts
+++ src/modules/doctor/useCases/create-doctor/create-doctor.controller.ts
@@ -1,8 +1,11 @@
+import { z } from "zod";
 import { Request, Response } from "express";
 import { ISpecialityRepository } from "../../../speciality/repositories/ispeciality.repository";
 import { IUserRepository } from "../../../users/repositories/iuser.repository";
 import { IDoctorRepository } from "../../repositories/idoctor.repository";
 import { CreateDoctorUseCase } from "./create-doctor.usecase";
+import { validatorSchema } from "../../../../infra/shared/validator/zod";
+import { ValidationSchemaError } from "../../../../error/validation-schema.error";
 
 export class CreateDoctorController {
   constructor(
@@ -14,14 +17,37 @@ export class CreateDoctorController {
   async handle(request: Request, response: Response): Promise<Response> {
     const { body } = request;
 
-    const createDoctorUseCase = new CreateDoctorUseCase(
-      this.userRepository,
-      this.doctorRepository,
-      this.specialityRepository
-    );
+    const doctorSchema = z.object({
+      name: z.string(),
+      username: z.string(),
+      email: z.string().email(),
+      password: z.string(),
+      crm: z.string().length(6, {
+        message: 'CRM must contain 6 characters'
+      }),
+      specialityId: z.string().uuid({
+        message: 'You need to insert a valid speciality ID'
+      })
+    });
 
-    const doctorCreated = await createDoctorUseCase.execute(body);
+    try {
+      validatorSchema(doctorSchema, body);
 
-    return response.json(doctorCreated);
+      const createDoctorUseCase = new CreateDoctorUseCase(
+        this.userRepository,
+        this.doctorRepository,
+        this.specialityRepository
+      );
+
+      const doctorCreated = await createDoctorUseCase.execute(body);
+
+      return response.json(doctorCreated);
+
+    } catch (err: any) {
+      if (err instanceof ValidationSchemaError) {
+        return response.status(err.statusCode).json(err.errors);
+      }
+      return response.status(err.statusCode).json(err.message);
+    }
   }
 }
